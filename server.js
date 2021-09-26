09d3991238c7db550993157a41f1f98104c63944
Chandler Liang <chandler.liang123@gmail.com>
2021-09-26 19:35:09 +1000

    created the relevant starting files

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// Middleware
+app.use(logger("dev"));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
